19fc39db83e615f4640aea24b31faca9d46e883d
maqunboy <markexin@sina.com>
2019-04-22 20:52:30 +0800

    feat: v1.0

---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "PRIVATE_KEY" : "5Xr3p0upUdjyn7Gksj6NoHY5A8A5U8dL"
+}
--- webpack-image-compression.js
+++ webpack-image-compression.js
@@ -1,23 +1,109 @@
 /*
  * @Description: webpack 打包编译图片压缩
- * @Author: maqun02
- * @LastAuthor: maqun02
+ * @Author: maqun
+ * @LastAuthor: maqun
  * @since: 2019-04-18 16:56:15
- * @lastTime: 2019-04-18 17:00:49
+ * @lastTime: 2019-04-22 20:48:14
  */
+const fs = require('fs');
+const path = require('path');
+const tinify = require("tinify");
+const config = require('./config/key');
 
-class webpackImageCompression {
+// 工具类
+class utils {
+
+    filePathMethod (fileString) {
+        return path.resolve(__dirname + (fileString || ''));
+    }
+
+    isImage (file) {
+        return (/\.(png|jpe?g|gif|svg)(\?.*)?$/).test(file);
+    }
+
+    timeFormat (time) {
+        const date = new Date(time),
+        Y = date.getFullYear() + '-',
+        M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-',
+        D = date.getDate() + ' ',
+        h = date.getHours() + ':',
+        m = date.getMinutes() + ':',
+        s = date.getSeconds()
+        return Y + M + D + h + m + s;
+    }
+
+}
+
+// 主程序
+class webpackImageCompression extends utils {
+
+    constructor (options) {
+        super(options);
+        this.cache = {};
+        this.props = options;
+        // 默认每个月500张
+        tinify.key = (options && options.key) || config.PRIVATE_KEY;
+    }
+
+    reWrite () {
+        // 复写缓存cache
+        fs.writeFile(this.filePathMethod('/.imageCache.js'), `module.exports = ${JSON.stringify(this.cache)}`);
+    }
+
+    fileDisplay (filePath) {
+        // 根据文件路径读取文件，返回文件列表
+        fs.readdir(filePath, { encoding: 'utf8' }, (err, files) => {
+
+            if(err) throw err;
+            
+            // 遍历读取到的文件列表
+            files.forEach( filename => {
+                // 获取当前文件的绝对路径
+                let fileAbsoluteAddress = path.join(filePath, filename);
+                // 根据文件路径获取文件信息，返回一个fs.Stats对象
+                fs.stat(fileAbsoluteAddress, (error, stats) => {
+                    if(err) throw error;
+
+                    const isFile = stats.isFile();
+                    const isDir = stats.isDirectory();
+                    const { compressDir } = this.props;
+                    const num = `${filePath}/${filename}`.indexOf(`${compressDir}`);
+                    const cacheKey = `${filePath}/${filename}`.substring(num);
+                    if(isFile && this.isImage(filename) && !Object.prototype.hasOwnProperty.call(this.cache, cacheKey)) {
+                        // 更新缓存
+                        this.cache[cacheKey] = this.timeFormat(new Date());
+                        tinify.fromFile(`${filePath}/${filename}`).toFile(`${filePath}/${filename}`);
+                    }
+                    // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
+                    if(isDir) 
+                        this.fileDisplay(fileAbsoluteAddress);
+                })
+            });
+        });
+        
+    }
+
     apply (compiler) {
         // 指定要附加到的事件钩子函数
         compiler.hooks.emit.tapAsync('webpackImageCompression', (compilation, callback) => {
-                console.log('This is an example plugin!');
-                console.log('Here’s the `compilation` object which represents a single build of assets:', compilation);
-                // 使用 webpack 提供的 plugin API 操作构建结果
-                compilation.addModule(/* ... */);
+                // 判断是否存在 cache 镜像 
+                try {
+                    const imageCache = require('./.imageCache');
+                    this.cache = imageCache;
+                } catch (error) {
+                    fs.writeFile(this.filePathMethod('/.imageCache.js'), 'module.exports = {}');
+                }
+                // 调用递归
+                this.fileDisplay(this.filePathMethod(`/${this.props.compressDir}`));
                 callback();
             }
         );
+
+         // 异步的事件钩子
+        compiler.plugin('afterEmit', () => this.reWrite());
+
     }
+    
 }
 
 
