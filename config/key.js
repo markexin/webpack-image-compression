ceef31c4c7f9f8b5017c44e5c61eda1a9c361e87
maqun02 <maqun02@58.com>
2020-11-24 17:16:03 +0800

    fix: 更新内部创建文件BUG

--- config/key.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    "PRIVATE_KEY" : "5Xr3p0upUdjyn7Gksj6NoHY5A8A5U8dL"
-}
--- webpack-image-compression.js
+++ webpack-image-compression.js
@@ -1,82 +1,80 @@
 /*
  * @Description: webpack 打包编译图片压缩
  * @Author: maqun
- * @LastAuthor: maqun
+ * @LastAuthor: Please set LastEditors
  * @since: 2019-04-18 16:56:15
- * @lastTime: 2019-04-24 17:19:59
+ * @lastTime: 2020-11-24 17:12:53
  */
 const fs = require('fs');
 const path = require('path');
 const tinify = require("tinify");
-const config = require('./config/key');
+const crypto = require('crypto');
+const md5 = crypto.createHash('md5');
 
-// 工具类
-class utils {
 
-    filePathMethod (fileString) {
-        return path.resolve(process.cwd() + (fileString || ''));
-    }
+const BASE_TINIFY_KEY = "5Xr3p0upUdjyn7Gksj6NoHY5A8A5U8dL";
+const MD_KEY = "webpackImageCompression";
 
-    isImage (file) {
-        return (/\.(png|jpe?g|gif|svg)(\?.*)?$/).test(file);
-    }
+/**
+ * 创建本地缓存文件
+ * @param {string} source 创建文件路径
+ */
+function createCacheFile (source) {
+    fs.writeFileSync(path.resolve(process.cwd() + (source || '')), "");
+    return ;
+}
 
-    timeFormat (time) {
-        const date = new Date(time),
-        Y = date.getFullYear() + '-',
-        M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-',
-        D = date.getDate() + ' ',
-        h = date.getHours() + ':',
-        m = date.getMinutes() + ':',
-        s = date.getSeconds()
-        return Y + M + D + h + m + s;
-    }
-
+/**
+ * 判断图片格式
+ * @param {string} file 文件路径
+ */
+function isImage (file) {
+    return (/\.(png|jpe?g|gif|svg)(\?.*)?$/).test(file);
 }
 
 // 主程序
-class webpackImageCompression extends utils {
+class webpackImageCompression {
 
     constructor (options) {
-        super(options);
-        this.cache = {};
-        this.props = options;
-        // 默认每个月500张
-        tinify.key = (options && options.key) || config.PRIVATE_KEY;
+        // default
+        this.options = options || {
+            compressDir: "src",
+            key: BASE_TINIFY_KEY
+        };
+
+        tinify.key = this.options.key;
+    
     }
 
-    reWrite () {
-        // 复写缓存cache
-        fs.writeFile(this.filePathMethod('/.imageCache.js'), `module.exports = ${JSON.stringify(this.cache)}`);
-    }
-
-    fileDisplay (filePath) {
+    tinyCompress (filePath) {
         // 根据文件路径读取文件，返回文件列表
         fs.readdir(filePath, { encoding: 'utf8' }, (err, files) => {
 
             if(err) throw err;
             
             // 遍历读取到的文件列表
-            files.forEach( filename => {
+            files.forEach(filename => {
                 // 获取当前文件的绝对路径
-                let fileAbsoluteAddress = path.join(filePath, filename);
+                const fileAbsoluteAddress = path.join(filePath, filename);
                 // 根据文件路径获取文件信息，返回一个fs.Stats对象
                 fs.stat(fileAbsoluteAddress, (error, stats) => {
+                    
                     if(err) throw error;
 
                     const isFile = stats.isFile();
                     const isDir = stats.isDirectory();
-                    const { compressDir } = this.props;
-                    const num = `${filePath}/${filename}`.indexOf(`${compressDir}`);
-                    const cacheKey = `${filePath}/${filename}`.substring(num);
-                    if(isFile && this.isImage(filename) && !Object.prototype.hasOwnProperty.call(this.cache, cacheKey)) {
+
+                    if (isDir) {
+                        this.tinyCompress(fileAbsoluteAddress);
+                        return
+                    }
+                    
+                    if(isFile && isImage(filename)) {
+                        const hashPassd = md5.update(filename).digest(MD_KEY);
                         // 更新缓存
-                        this.cache[cacheKey] = this.timeFormat(new Date());
                         tinify.fromFile(`${filePath}/${filename}`).toFile(`${filePath}/${filename}`);
                     }
-                    // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
-                    if(isDir) 
-                        this.fileDisplay(fileAbsoluteAddress);
+
                 })
             });
         });
@@ -86,21 +84,23 @@ class webpackImageCompression extends utils {
     apply (compiler) {
         // 指定要附加到的事件钩子函数
         compiler.hooks.emit.tapAsync('webpackImageCompression', (compilation, callback) => {
-                // 判断是否存在 cache 镜像 
-                try {
-                    const imageCache = require('./.imageCache');
-                    this.cache = imageCache;
-                } catch (error) {
-                    fs.writeFile(this.filePathMethod('/.imageCache.js'), 'module.exports = {}');
-                }
-                // 调用递归
-                this.fileDisplay(this.filePathMethod(`/${this.props.compressDir}`));
+
+                fs.access(path.resolve(`${process.cwd()}/.imageCache.md`), fs.constants.F_OK, (err) => {
+
+                    if (err) {
+                        createCacheFile('/.imageCache.md');
+                    }
+
+                    // 执行压缩
+                    this.tinyCompress(
+                        path.resolve(process.cwd() + `/${this.options.compressDir}`)
+                    );
+
+                });
+
                 callback();
             }
         );
-
-         // 异步的事件钩子
-        compiler.plugin('afterEmit', () => this.reWrite());
 
     }
     
